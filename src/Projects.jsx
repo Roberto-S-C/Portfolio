9a46fc215698ece5c70ef1ee1a21a0b5d5f73bd4
Roberto-S-C <robertosanchezcazares@gmail.com>
2025-04-23 08:34:32 -0600

    Added Link to Project

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -12,7 +12,7 @@ function Projects() {
                 </div>
             </section>
 
-            <h2 id='title' className="text-center text-5xl font-bold font-newsreader">ActiveX</h2>
+            <Link to={"https://www.activex.club"} id='title' className="text-center text-5xl font-bold font-newsreader">ActiveX</Link>
 
             <section className='flex flex-col lg:flex-row items-center w-11/12 h-3/4 lg:h-auto'>
                 <div id='video' className='flex justify-center w-full lg:w-1/2 mx-2'>
